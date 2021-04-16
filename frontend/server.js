ab1f55751d29f08c247ed52da020504e84386f8a
Efe Karakus <efekarakus@gmail.com>
2021-04-16 13:20:35 -0700

    use secretsmanager

--- frontend/server.js
+++ frontend/server.js
@@ -9,7 +9,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/hello', (req, res) => {
-  console.log("received request 24")
+  console.log("received request 24");
+  console.log(process.env.DB_USERNAME);
   res.send('Hello World');
   //res.status(400).send({error: 'boom'});
 });
