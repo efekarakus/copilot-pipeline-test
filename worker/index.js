345b6453d78c5f596af4635516a42ae6f459041d
Efe Karakus <efekarakus@gmail.com>
2021-08-26 10:22:20 -0700

    update with async

--- worker/index.js
+++ worker/index.js
@@ -3,25 +3,27 @@ const client = new SQSClient({ region: "us-west-2" });
 
 const {eventsQueue} = JSON.parse(process.env.COPILOT_QUEUE_URIS);
 
-console.log(`The queue url created is: ${eventsQueue}`);
-while(true) {
-    try {
-        const out = await client.send(new ReceiveMessageCommand({
-            QueueUrl: eventsQueue,
-            WaitTimeSeconds: 10,
-        }));
-
-        console.log(`results: ${JSON.stringify(out)}`);
-
-        if (out.Messages.length === 0) {
-            continue;
+(async () => {
+    console.log(`The queue url created is: ${eventsQueue}`);
+    while(true) {
+        try {
+            const out = await client.send(new ReceiveMessageCommand({
+                QueueUrl: eventsQueue,
+                WaitTimeSeconds: 10,
+            }));
+    
+            console.log(`results: ${JSON.stringify(out)}`);
+    
+            if (out.Messages.length === 0) {
+                continue;
+            }
+    
+            await client.send( new DeleteMessageCommand({
+                QueueUrl: eventsQueue,
+                ReceiptHandle: out.Messages[0].ReceiptHandle,
+            }));
+        } catch (err) {
+            console.error(err);
         }
-
-        await client.send( new DeleteMessageCommand({
-            QueueUrl: eventsQueue,
-            ReceiptHandle: out.Messages[0].ReceiptHandle,
-        }));
-    } catch (err) {
-        console.error(err);
     }
-}
+})();
